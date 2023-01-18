1047e14ad4936d5f9b21869fed5c2e7f4c40048c
Luiz Gustavo <gusstavopdm@gmail.com>
2023-01-17 22:40:32 -0300

    feat: add route post /habits

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,27 @@
+import dayjs from 'dayjs';
 import { FastifyInstance } from 'fastify';
+import { prisma } from './lib/prisma';
+import { z } from 'zod';
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    return 'Hello World!';
+  app.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+    const today = dayjs().startOf('day').toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map((day) => ({ week_day: day })),
+        },
+      },
+    });
   });
 }
